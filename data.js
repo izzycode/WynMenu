3915633aa911d001dc9c7d2a4a30a03a8f01f462
Israel Martinez <marcemc@yahoo.com>
2019-06-25 21:24:23 -0400

    Wyn Menu completed

---
+++ data.js
@@ -0,0 +1,7 @@
+
+let croqueta = new MenuItem('fusion croqueta', 325)
+let reuben = new Sandwich('hipster reuben', 1345)
+let club = new Sandwich('SoBe club', 1099)
+let italian = new Sandwich('Italian', 950)
+
+let menuItems = [ croqueta, reuben, club, italian ]
